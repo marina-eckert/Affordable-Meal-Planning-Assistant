3f163d7f9661c6727a305ebcdcdcbd4cc4d160e0
Hamza Rehman <72849282+HamzaRehmann@users.noreply.github.com>
2025-10-24 16:41:34 +0200

    added auth routes

--- mp-assistant/src/routes/AppRoutes.jsx
+++ mp-assistant/src/routes/AppRoutes.jsx
@@ -1,26 +1,49 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import MainLayout from "../layouts/MainLayout";
+import AuthLayout from "../layouts/AuthLayout";
 
 import Dashboard from "../pages/Dashboard";
 import Profile from "../pages/Profile";
 import GroceryList from "../pages/GroceryList";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
 
 const AppRoutes = () => {
   return (
     <Routes>
-      <Route
-        path="/dashboard"
-        element={
-          <MainLayout>
-            <Dashboard />
-          </MainLayout>
-        }
-      />
+      <Route path="/login" element={
+        <AuthLayout>
+          <Login />
+        </AuthLayout>
+      } />
+      
+      <Route path="/register" element={
+        <AuthLayout>
+          <Register />
+        </AuthLayout>
+      } />
+      
+      <Route path="/dashboard" element={
+        <MainLayout>
+          <Dashboard />
+        </MainLayout>
+      } />
 
-      <Route path="/profile" element={<Profile />} />
-      <Route path="/grocery" element={<GroceryList />} />
+      <Route path="/profile" element={
+        <MainLayout>
+          <Profile />
+        </MainLayout>
+      } />
+
+      <Route path="/grocery" element={
+        <MainLayout>
+          <GroceryList />
+        </MainLayout>
+      } />
+
     </Routes>
   );
 };
 
 export default AppRoutes;
+
